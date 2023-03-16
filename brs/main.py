41abde983c59625442e9271e55098d8b3b6eda38
fredericokremer <fred.s.kremer@hotmail.com>
2023-03-16 12:46:54 -0300

    updated protein file

--- brs/main.py
+++ brs/main.py
@@ -79,8 +79,11 @@ def run_converter_gbk_to_protein_fna (input_gbk_file, output_fna_file):
     for seq_record in SeqIO.parse(open(input_gbk_file), "genbank"):
         for seq_feature in seq_record.features :
             if seq_feature.type=="CDS" :
-                output_handle.write(">%s\n%s\n" % (
+                output_handle.write(">%s %s:%s:%s\n%s\n" % (
                     seq_feature.qualifiers['locus_tag'][0],
+                    seq_record.id,
+                    str(seq_feature.location.start+1),
+                    str(seq_feature.location.end),
                     str(seq_feature.location.extract(seq_record.seq).translate())))
                 
     output_handle.close()
